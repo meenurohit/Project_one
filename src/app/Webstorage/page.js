d6b22f96e2d281ad6526797c3348b8561fddedc8
meenurohit <meenakshi.sharma1989@gmail.com>
2025-02-21 01:39:15 +0530

    third

--- src/app/Webstorage/page.js
+++ src/app/Webstorage/page.js
@@ -4,25 +4,7 @@ import React, { useState } from 'react';
 import Pagination from '@/Components/Pagination';
 
 const WebStorage = () => {
-  const [localValue, setLocalValue] = useState(localStorage.getItem('lastname') || '');
-  const [sessionValue, setSessionValue] = useState(sessionStorage.getItem('clickcount') || 0);
-
-  const storeLocalStorage = () => {
-    localStorage.setItem("lastname", "Smith");
-    setLocalValue("Smith");
-  };
-
-  const removeLocalStorage = () => {
-    localStorage.removeItem("lastname");
-    setLocalValue('');
-  };
-
-  const incrementSessionStorage = () => {
-    const count = sessionStorage.getItem("clickcount") ? Number(sessionStorage.getItem("clickcount")) + 1 : 1;
-    sessionStorage.setItem("clickcount", count);
-    setSessionValue(count);
-  };
-
+ 
   return (
     <div className="content-html mt-8 mx-auto max-w-4xl p-6 bg-white text-gray-900 rounded-2xl shadow-md">
       <div className="heading mb-6">
@@ -52,16 +34,16 @@ const WebStorage = () => {
 
         <h3 className="text-lg font-semibold mb-2">Interactive localStorage Example</h3>
         <div className="mb-4">
-          <button onClick={storeLocalStorage} className="px-4 py-2 bg-green-500 text-white rounded-md mr-2">Store Data</button>
-          <button onClick={removeLocalStorage} className="px-4 py-2 bg-red-500 text-white rounded-md">Remove Data</button>
+          <button className="px-4 py-2 bg-green-500 text-white rounded-md mr-2">Store Data</button>
+          <button  className="px-4 py-2 bg-red-500 text-white rounded-md">Remove Data</button>
         </div>
-        <p className="font-semibold">Stored Value: <span className="text-blue-600">{localValue || "No Data"}</span></p>
+        <p className="font-semibold">Stored Value: <span className="text-blue-600"></span></p>
 
         <h3 className="text-lg font-semibold mt-6 mb-2">Interactive sessionStorage Example</h3>
         <div className="mb-4">
-          <button onClick={incrementSessionStorage} className="px-4 py-2 bg-blue-500 text-white rounded-md">Click to Increment</button>
+          <button  className="px-4 py-2 bg-blue-500 text-white rounded-md">Click to Increment</button>
         </div>
-        <p className="font-semibold">Click Count (Session): <span className="text-blue-600">{sessionValue}</span></p>
+        <p className="font-semibold">Click Count (Session): <span className="text-blue-600"></span></p>
 
       </div>
     </div>
